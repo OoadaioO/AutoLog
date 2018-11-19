9834fd7777b1bf32f9ed3ad3f45d9eaf3d5150cb
xubo <undefine@qq.com>
2018-11-19 16:04:45 +0800

    bugfix:方法访问权限

--- log/src/main/java/cc/kaipao/dongjia/log/TimeCache.java
+++ log/src/main/java/cc/kaipao/dongjia/log/TimeCache.java
@@ -18,44 +18,20 @@ public class TimeCache {
     private static Map<String, Long> startTimeMap = new HashMap<>();
     private static Map<String, Long> endTimeMap = new HashMap<>();
 
-    private static String getCostTime(String methodName) {
+    public static String getCostTime(String methodName) {
         long startTime = startTimeMap.get(methodName);
         long endTime = endTimeMap.get(methodName);
         long detaTime = endTime - startTime;
         return methodName + " cost:" + detaTime + " ms";
     }
 
-    private static void setStartTime(String methodName, long time) {
+    public static void setStartTime(String methodName, long time) {
         startTimeMap.put(methodName, time);
     }
 
-    private static void setEndTime(String methodName, long time) {
+    public static void setEndTime(String methodName, long time) {
         endTimeMap.put(methodName, time);
     }
 
-
-    public static View.OnClickListener inject(final View.OnClickListener listener) {
-        return new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                setStartTime("onClick", System.currentTimeMillis());
-                listener.onClick(view);
-                setEndTime("onClick", System.currentTimeMillis());
-                Log.d("AutoLog", getCostTime("onClick"));
-            }
-        };
-    }
-
-
-    public static void main(String[] args) {
-        View view = new View(null);
-        view.setOnClickListener(inject(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("test");
-            }
-        }));
-    }
 
 }
